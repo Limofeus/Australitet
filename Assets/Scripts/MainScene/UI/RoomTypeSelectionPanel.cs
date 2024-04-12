aa9d50701cff59273b65631f56c099fc4c3e345b
Limofeus <69484438+Limofeus@users.noreply.github.com>
2024-04-12 23:26:12 +0300

    Update RoomTypeSelectPanel.prefab, RoomTypeSelectionPanel.cs, TimTestScene.unity, and 1 more file

--- Assets/Scripts/MainScene/UI/RoomTypeSelectionPanel.cs
+++ Assets/Scripts/MainScene/UI/RoomTypeSelectionPanel.cs
@@ -12,12 +12,14 @@ public class RoomTypeSelectionPanel : MonoBehaviour
     public EmptyRoom room;
     public float zoomFacMin;
     public float zoomFacMax;
+    private float lastFrameDist = 0f;
     private void Start()
     {
         panelTransform.localScale = Vector3.zero;
     }
     void Update()
     {
+        float curDist = Mathf.Abs(Input.mousePosition.x - panelTransform.position.x);
         float zoomFac = Mathf.Lerp(zoomFacMin, zoomFacMax, CameraMover._currentZoom);
         Vector3 lerpTarget = Vector3.zero;
         if (panelOpened)
@@ -25,7 +27,7 @@ public class RoomTypeSelectionPanel : MonoBehaviour
             lerpTarget = Vector3.one;
         }
         panelTransform.localScale = Vector3.Lerp(panelTransform.localScale, lerpTarget * zoomFac, 1 - Mathf.Exp(-lerpPower * Time.deltaTime));
-        if (Mathf.Abs(Input.mousePosition.x - panelTransform.position.x) > (panelTransform.sizeDelta.x / 2f * zoomFac) || Mathf.Abs(Input.mousePosition.y - panelTransform.position.y) > (panelTransform.sizeDelta.y / 2f * zoomFac))
+        if ((Mathf.Abs(Input.mousePosition.x - panelTransform.position.x) > (panelTransform.sizeDelta.x / 2f * zoomFac) || Mathf.Abs(Input.mousePosition.y - panelTransform.position.y) > (panelTransform.sizeDelta.y / 2f * zoomFac)) && lastFrameDist < curDist)
         {
             if (panelOpened)
             {
@@ -37,6 +39,7 @@ public class RoomTypeSelectionPanel : MonoBehaviour
         {
             //panelOpened = true;
         }
+        lastFrameDist = curDist;
     }
     public void SelectRoom(int roomId)
     {
