57298c66d3b997fdb2aec33f0d556aa92bc61024
Damir <damir2004damir@mail.ru>
2024-04-13 11:39:57 +0300

    Castle

--- Assets/Scripts/UI/Rooms/RecreationRoomPanelUI.cs
+++ Assets/Scripts/UI/Rooms/RecreationRoomPanelUI.cs
@@ -1,12 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class RecreationRoomPanelUI : RoomPanelUI
 {
     public Research room;
+    [SerializeField] private Slider _curePeople;
+
     public override void ActivateResearch()
     {
         
+    }
+
+    private void OnEnable()
+    {
+        Debug.Log("asdad");
     }
 }
