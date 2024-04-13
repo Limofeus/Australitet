139f7d8ed5818fd2df169f1ef982ec3c0f40c6f9
Limofeus <69484438+Limofeus@users.noreply.github.com>
2024-04-13 16:39:00 +0300

    Update CameraMover.cs, BigEvent.cs, BigTestEvent.cs, and 6 more files

---
+++ Assets/Scripts/MainScene/Events/SmallTestEvent.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SmallTestEvent : SmallEvent
+{
+    public override bool IsPossible()
+    {
+        return true;
+    }
+    public override void OnDayStart()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void OnDayEnd()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public SmallTestEvent(string evName)
+    {
+        eventName = evName;
+    }
+}
